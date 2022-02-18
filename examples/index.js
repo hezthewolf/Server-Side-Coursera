f1e91e4c5bb0d5af8645d5507314b0226d3a6e57
Hez <mokuahesbon254@gmail.com>
2022-02-18 17:46:49 +0300

    Initial commit

---
+++ examples/index.js
@@ -0,0 +1,23 @@
+const rect = {
+  perimeter: (x, y) => 2 * (x + y),
+  area: (x, y) => x * y,
+};
+
+const solveRect = (a, b) => {
+  console.log(`Perform arithmetic operations on: ${a} and ${b}`);
+
+  if (a <= 0 || b <= 0) {
+    console.log("Value cannot be less than or equal to zero");
+  } else {
+    console.log(`The perimeter of the rectangle is: ${rect.perimeter(a, b)}`);
+    console.log(`The area of the rectangle is: ${rect.area(a, b)}`);
+  }
+
+  console.log("---------------------------------------");
+};
+
+solveRect(3, 5);
+solveRect(4, 10);
+solveRect(2, 3);
+solveRect(-1, 5);
+solveRect(3, 8);
