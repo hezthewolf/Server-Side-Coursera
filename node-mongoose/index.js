ca574b78a3c7425a0d6934b660656a758f329e17
Hez <mokuahesbon254@gmail.com>
2022-03-28 06:48:29 +0300

    Mongoose Part 2

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -1,33 +1,32 @@
-const mongoose =  require('mongoose')
+const mongoose = require('mongoose')
 
 const Dishes = require('./models/dishes')
 
 const url = 'mongodb://localhost:27017/conFusion'
 const connect = mongoose.connect(url)
 
-connect.then((db) => { 
+connect.then((db) => {
     console.log('Connected correctly to server')
 
-    const newDish = Dishes({
+    Dishes.create({
         name: 'Uthapizza',
         description: 'test'
     })
 
-    newDish.save()
-        .then((dish) => {
-            console.log(dish)
+    .then((dish) => {
+        console.log(dish)
 
-            return Dishes.find({}).exec()
-        })
-        .then((dishes) => {
-            console.log(dishes)
+        return Dishes.find({}).exec()
+    })
+    .then((dishes) => {
+        console.log(dishes)
 
-            return Dishes.deleteOne({})
-        })
-        .then(() => {
-            return mongoose.connection.close()
-        })
-        .catch((err) => { 
-            console.log(err)
-        })
+        return Dishes.deleteOne({})
+    })
+    .then(() => {
+        return mongoose.connection.close()
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 })
